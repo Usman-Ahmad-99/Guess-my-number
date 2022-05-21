75ced7c9aefb325411511d2e088a126b160dde9b
Usman-Ahmad-99 <usman.ahmad.9269@gmail.com>
2022-05-21 15:35:43 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing..';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
